8bd824e024e653c627dd028787ee84a428fae52f
Forbzi <dvasutov2018@gmail.com>
2025-02-15 15:43:01 +0300

    Эхослем в Турцию

---
+++ src/main/java/org/example/edu/controller/MainController.java
@@ -0,0 +1,16 @@
+package org.example.edu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name) {
+        return "Hello" + name;
+    }
+
+}
